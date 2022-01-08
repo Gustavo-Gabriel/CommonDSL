484012124ba59cc0ee028ab7eb5d12faf2b29e81
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2022-01-07 21:16:53 -0400

    Initial Commit

---
+++ CommonDSL/CommonDSL.h
@@ -0,0 +1,18 @@
+//
+//  CommonDSL.h
+//  CommonDSL
+//
+//  Created by Gustavo Anjos on 07/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonDSL.
+FOUNDATION_EXPORT double CommonDSLVersionNumber;
+
+//! Project version string for CommonDSL.
+FOUNDATION_EXPORT const unsigned char CommonDSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonDSL/PublicHeader.h>
+
+
